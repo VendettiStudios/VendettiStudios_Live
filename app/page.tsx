446db423945b3fc8741a390da85e9bdfe95ba286
DH <daniel@vendettistudios.com>
2023-07-28 14:28:46 -0700

    Next Upgrade, SEO

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,142 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Group, Box3, Vector3, Object3D } from 'three'
+import { Html, PerspectiveCamera, useGLTF } from '@react-three/drei'
+import { Suspense, useRef, useEffect, useState } from 'react'
+import styles from './home.module.css'
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+function Model() {
+    const gltf = useGLTF('/VS3DLOGO2.gltf', true)
+    const group = useRef<Group>(null)
+    const pivot = useRef<Object3D>(null)
+
+    useEffect(() => {
+        if (group.current && pivot.current) {
+            const box = new Box3().setFromObject(group.current)
+            const center = new Vector3()
+            box.getCenter(center)
+            group.current.position.sub(center)
+            pivot.current.add(group.current)
+        }
+    }, [])
+
+    useFrame(({ clock }) => {
+        if (pivot.current) {
+            pivot.current.rotation.x = Math.PI / 2
+            pivot.current.rotation.z = clock.getElapsedTime()
+        }
+    })
+
+    return (
+        <object3D ref={pivot}>
+            <group ref={group} dispose={null} rotation-z={Math.PI} scale={3}>
+                <primitive object={gltf.scene} />
+            </group>
+        </object3D>
+    )
+}
+
+
+function SpinningLogo() {
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            const resizeObserver = new ResizeObserver(() => {
+                if (canvasRef.current) {
+                    canvasRef.current.style.width = `${window.innerWidth}px`
+                    canvasRef.current.style.height = `${window.innerHeight}px`
+                }
+            })
+
+            resizeObserver.observe(document.body)
+
+            return () => {
+                resizeObserver.disconnect()
+            }
+        }
+    }, [])
+
+    return (
+        <Canvas className={styles.fullscreen} ref={canvasRef}>
+            <Suspense fallback={null}>
+                <Model />
+            </Suspense>
+            <PerspectiveCamera makeDefault position={[0, 0, 2]} fov={50} />
+            <ambientLight intensity={1} />
+            <directionalLight position={[0, 10, 5]} intensity={4} />
+    </Canvas>
+    )
+}
+
+function useInterval(callback: () => void, delay: number | null) {
+    const savedCallback = useRef(callback)
+
+    useEffect(() => {
+        savedCallback.current = callback
+    })
+
+    useEffect(() => {
+        if (delay !== null) {
+            const id = setInterval(() => savedCallback.current(), delay)
+            return () => clearInterval(id)
+        }
+    }, [delay])
+}
+function CountdownTimer() {
+    const endDate = new Date('2023-06-03T00:00:00'); // Set a fixed end date
+    const [timeLeft, setTimeLeft] = useState<number | null>(null);
+  
+    useEffect(() => {
+      setTimeLeft(Math.floor((endDate.getTime() - new Date().getTime()) / 1000));
+    }, []);
+  
+    useInterval(() => {
+      if (timeLeft !== null && timeLeft > 0) {
+        setTimeLeft(timeLeft - 1);
+      }
+    }, 1000);
+  
+    if (timeLeft === null) {
+      return <div className={styles.countdown}>Loading...</div>; // Render a placeholder while waiting for client-side hydration
+    }
+    const days = Math.floor(timeLeft / (24 * 60 * 60))
+    const hours = Math.floor((timeLeft % (24 * 60 * 60)) / (60 * 60))
+    const minutes = Math.floor((timeLeft % (60 * 60)) / 60)
+    const seconds = timeLeft % 60
+
+    return (
+        <div className={styles.countdown}>
+          <div className={styles.timeUnit}>
+            <span>{String(days).padStart(2, '0')}</span>
+            <span>Days</span>
+          </div>
+          <div className={styles.timeUnit}>
+            <span>{String(hours).padStart(2, '0')}</span>
+            <span>Hours</span>
+          </div>
+          <div className={styles.timeUnit}>
+            <span>{String(minutes).padStart(2, '0')}</span>
+            <span>Minutes</span>
+          </div>
+          <div className={styles.timeUnit}>
+            <span>{String(seconds).padStart(2, '0')}</span>
+            <span>Seconds</span>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      );
+    }
+export default function Home() {
+    return (
+      <>
+        {/* ... Head component ... */}
+        <main className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.spinningLogo}><SpinningLogo  /></div>
+          <h1 className={styles.comingSoon}>Coming Soon!</h1>
+          {/* <CountdownTimer /> */}
+        </div>
+      </main>
+    </>
   )
 }
