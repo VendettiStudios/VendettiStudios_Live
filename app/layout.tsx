43e28ad014d952eb31b912fa4c717a7d97a86539
Daniel Holloway <daniel@vendettistudios.com>
2025-03-29 12:14:52 -0700

    clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-
+import Script from "next/script";
 const inter = Inter({ subsets: ['latin'] });
 
 // Define the metadata including the title and description
@@ -32,6 +32,22 @@ export default function RootLayout({ children }: RootLayoutProps) {
       </head>
       <body className={inter.className}>
         {children}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+   (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "qvxrhapu0z");
+
+    // Force Clarity to capture the full page
+    window.clarity = window.clarity || function() {
+      (window.clarity.q = window.clarity.q || []).push(arguments);
+    };
+
+    clarity("set", "pageView", { view: "full" });
+  `}
+        </Script>
       </body>
     </html>
   );
